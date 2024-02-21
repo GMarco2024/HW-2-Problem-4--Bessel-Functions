332fe8e0e2b8e9e39927aca640932e0f7915ff99
GMarco2024 <mgonzalez11@hawk.iit.edu>
2024-02-21 07:34:46 -0800

    Split Problem4a.swift into two files for organizational purposes.

--- HW 2 Problem 4- Bessel Functions/DownwardRecursion.swift
+++ HW 2 Problem 4- Bessel Functions/DownwardRecursion.swift
@@ -1,33 +1,11 @@
 //
-//  Problem4a.swift
-//  HW2 Problem 4 - Bessel Function
+//  DownwardRecursion.swift
+//  HW 2 Problem 4- Bessel Functions
 //
-//  Modified by Marco Gonzalez 2/20/24
+//  Created by Marco on 2/21/24.
 //
 
 import Foundation
-
-func calculateUpwardRecursion(x: Double, lMax: Int) -> [Double] {
-    var results = [Double](repeating: 0.0, count: lMax + 1)
-    
-  
-    if x == 0 {
-        results[0] = 1.0
-        return results
-    }
-    
-    results[0] = sin(x) / x
-    if lMax >= 1 {
-        results[1] = sin(x) / (x*x) - cos(x) / x
-    }
-    
-    // Upward recursion
-    for l in 1..<lMax {
-        results[l + 1] = ((2.0 * Double(l) + 1.0) / x) * results[l] - results[l - 1]
-    }
-    
-    return results
-}
 
 func calculateDownwardRecursion(x: Double, lMax: Int) -> [Double] {
     var results = [Double](repeating: 0.0, count: lMax + 2) // Extra space for initial guess
@@ -36,13 +14,11 @@ func calculateDownwardRecursion(x: Double, lMax: Int) -> [Double] {
     results[lMax + 1] = 0.0
     results[lMax] = 1.0
     
-    // Downward recursion
     for l in (0..<lMax).reversed() {
         results[l] = ((2.0 * Double(l) + 1.0) / x) * results[l + 1] - results[l + 2]
     }
     
     // Normalization with j0(x) since downward recursion does not guarantee correct magnitude
-    
     let scale = sin(x) / x / results[0]
     for l in 0...lMax {
         results[l] *= scale
---
+++ HW 2 Problem 4- Bessel Functions/UpwardRecursion.swift
@@ -0,0 +1,29 @@
+//
+//  UpwardRecursion.swift
+//  HW 2 Problem 4- Bessel Functions
+//
+//  Created by Marco on 2/21/24.
+//
+
+import Foundation
+
+func calculateUpwardRecursion(x: Double, lMax: Int) -> [Double] {
+    var results = [Double](repeating: 0.0, count: lMax + 1)
+    
+    if x == 0 {
+        results[0] = 1.0
+        return results
+    }
+    
+    results[0] = sin(x) / x
+    if lMax >= 1 {
+        results[1] = sin(x) / (x*x) - cos(x) / x
+    }
+    
+    for l in 1..<lMax {
+        results[l + 1] = ((2.0 * Double(l) + 1.0) / x) * results[l] - results[l - 1]
+    }
+    
+    return results
+}
+
