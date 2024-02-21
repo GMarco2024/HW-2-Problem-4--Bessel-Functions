af4fa384be7df460b03b6ff5b2a566904318c5b7
GMarco2024 <mgonzalez11@hawk.iit.edu>
2024-02-21 11:42:55 -0800

    Changes were made.

--- HW 2 Problem 4- Bessel Functions/DownwardRecursion.swift
+++ HW 2 Problem 4- Bessel Functions/DownwardRecursion.swift
@@ -12,18 +12,26 @@ import Foundation
 //     l - 1          x          l            l + 1
 
 
+
+// Downward Recursion for Spherical Bessel Functions
 func calculateDownwardRecursion(x: Double, lMax: Int) -> [Double] {
-    var results = [Double](repeating: 0.0, count: lMax + 2)
+    var results = [Double](repeating: 0.0, count: lMax + 2) // Extend array for initial guess
+
+    // Improved initial guess
     
-    // Initial guess for starting the recursion
     results[lMax + 1] = 0.0
     results[lMax] = 1.0
+    
+    // Execute downward recursion
     
     for l in (0..<lMax).reversed() {
         results[l] = ((2.0 * Double(l) + 1.0) / x) * results[l + 1] - results[l + 2]
     }
 
-    let scale = sin(x) / x / results[0]
+    // Precise normalization. this allows j0(x) to be most accurate for normalization
+    
+    let j0 = sin(x) / x
+    let scale = j0 / results[0]
     for l in 0...lMax {
         results[l] *= scale
     }
